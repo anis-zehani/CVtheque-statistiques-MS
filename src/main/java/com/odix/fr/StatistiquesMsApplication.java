ab06cf949720a66b69974f25ecb7b0191fddcd95
GitLabOdix <azaheni@smartgraphe.com>
2019-12-22 15:47:52 +0100

    first commit

---
+++ src/main/java/com/odix/fr/StatistiquesMsApplication.java
@@ -0,0 +1,13 @@
+package com.odix.fr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatistiquesMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatistiquesMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/odix/fr/StatistiquesMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.odix.fr;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class StatistiquesMsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
