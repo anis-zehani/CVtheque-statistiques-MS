5684d55282282180cc5711420f9d092a668f7cba
odix <odix@odix-laptop>
2020-04-27 21:05:11 +0100

    @FeignClient("http://backend-ms:8001")

--- src/main/java/com/odix/fr/webClients/TechnologieClient.java
+++ src/main/java/com/odix/fr/webClients/TechnologieClient.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 
 //@FeignClient(name = "backend-ms:8001", fallback = TechnologieClient.TechnologieClientFallback.class)
-@FeignClient(url = "http://backend-ms:8001", fallback = TechnologieClient.TechnologieClientFallback.class)
+@FeignClient("http://backend-ms:8001")
 public interface TechnologieClient {
 
 	@GetMapping("/api/technologie/getCountTechnologies")
