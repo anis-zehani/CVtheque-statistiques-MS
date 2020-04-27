b0a5cf47cb3110f2eff73f7aa655ac17039d54e3
odix <odix@odix-laptop>
2020-04-27 20:19:32 +0100

    @RibbonClient

--- src/main/java/com/odix/fr/webClients/TechnologieClient.java
+++ src/main/java/com/odix/fr/webClients/TechnologieClient.java
@@ -1,10 +1,12 @@
 package com.odix.fr.webClients;
 
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @FeignClient(name = "backend-ms:8001", fallback = TechnologieClient.TechnologieClientFallback.class)
+@RibbonClient(name = "backend-ms")
 public interface TechnologieClient {
 
 	@GetMapping("/api/technologie/getCountTechnologies")
