1768932f1dd83e71649c628fb51c75d65ce0ff0e
odix <odix@odix-laptop>
2020-04-27 07:03:50 +0100

    Suppression CrossOrigin

--- src/main/java/com/odix/fr/controller/StatistiquesController.java
+++ src/main/java/com/odix/fr/controller/StatistiquesController.java
@@ -3,14 +3,13 @@ package com.odix.fr.controller;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.odix.fr.service.StatistiquesService;
 
-@CrossOrigin
+
 @RestController
 @RequestMapping("/api/statistiques")
 public class StatistiquesController {
