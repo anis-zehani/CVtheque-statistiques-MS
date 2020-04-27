b6f1c8e30b585292b2a84a2a8657bc23ec2b45d6
odix <odix@odix-laptop>
2020-04-27 07:31:23 +0100

    @Scheduled(fixedRate = 300000)

--- src/main/java/com/odix/fr/service/StatistiquesServiceImpl.java
+++ src/main/java/com/odix/fr/service/StatistiquesServiceImpl.java
@@ -50,8 +50,8 @@ public class StatistiquesServiceImpl implements StatistiquesService {
 	}
 
 
-	// CRON ici qui fait appel aux 6 Web Clients Feign puis UPDATE les 6 chiffres clés dans la BDD : chaque 15 minutes
-	@Scheduled(fixedRate = 900000)
+	// CRON ici qui fait appel aux 6 Web Clients Feign puis UPDATE les 6 chiffres clés dans la BDD : chaque 5 minutes
+	@Scheduled(fixedRate = 300000)
 	public void cronUpdate6ChiffresClesViaWebClients() {
 		
 		//Initialiser à zéro pour éviter le nullPointer
